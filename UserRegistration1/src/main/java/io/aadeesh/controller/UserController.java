e28ff40804d0c64ac457a38c6e73694cb3d26490
SangyAdhikari <105117035+SangyAdhikari@users.noreply.github.com>
2022-08-27 15:36:37 +0530

    Update UserController.java

--- UserRegistration1/src/main/java/io/aadeesh/controller/UserController.java
+++ UserRegistration1/src/main/java/io/aadeesh/controller/UserController.java
@@ -12,11 +12,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController
 
 import io.aadeesh.model.User;
 import io.aadeesh.repo.UserRepository;
 
-@Controller
+@RestController
 public class UserController 
 {
 	@Autowired
